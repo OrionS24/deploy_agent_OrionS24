c9fe9a2fc72afb082d3a1fc8a2d9cb48cfc75219
Orion Seruvumba <o.seruvumba@alustudent.com>
2026-02-20 13:43:30 +0200

    Refactor attendance tracker script for clarity and fixes
    Fixed variable names and improved attendance threshold updates. Enhanced cleanup function and health check for Python3 installation.

--- MyScript.sh
+++ MyScript.sh
@@ -1,30 +1,37 @@
 bash
 #!/bin/bash
 read -p "Enter input name" input
+
 mkdir attendance_tracker_$input
 cd attendance_tracker_$input
-mkdir attendance_checker.py
-cd attendance_checker.py
+
+touch attendance_checker.py
+
 mkdir Helpers reports
-cd Helpers
-touch assets.csv config.json
-cd ..
-cd reports
-touch reports.log
-cd ..
-CONFIG_FILE="config.json"
+
+touch Helpers/assets.csv
+touch Helpers/config.json
+touch reports/reports.log
+
+cp ~/Downloads/attendance_checker.py attendance_checker.py
+cp ~/Downloads/assets.csv Helpers/assets.csv
+cp ~/Downloads/config.json Helpers/config.json
+cp ~/Downloads/reports.log reports/reports.log
+
+CONFIG_FILE="Helpers/config.json"
 
 echo "Do you want to update the attendance thresholds? (yes/no)"
 read choice
 
-if [ "answer" == "yes" ]; then
+if [ "choice" == "yes" ]; then
     read -p "Enter new warning threshold (defaut was 75): " warning
     read -p "Enter new failure threshold (defaut was 50): " failure
     warning=${warning:-75}
     failure=${failure:-50}
 
-    sed -i "s/\Wwarning_threshold\": *[0-9]*\/warning_threshold\": $warning/" $CONFIG_FILE
-    sed -i "s/\"failure_threshold\": *[0-9]*/\"failure_threshold\": $failure/" "$CONFIG_FILE"
+sed -i "s/\"warning_threshold\": *[0-9]*/\"warning_threshold\": $warning/" "$CONFIG_FILE"
+sed -i "s/\"failure_threshold\": *[0-9]*/\"failure_threshold\": $failure/" "$CONFIG_FILE"
+
     echo "Attendance thresholds updated successfully."
 else
     echo "Attendance thresholds remain unchanged."
@@ -38,6 +45,7 @@ archive_name="attendance_tracker_$input.tar.gz"
 
 mkdir -p "$project_dir"
 echo "Project directory created: $project_dir"
+
 cleanup(){
     echo ""
     echo "Cleanup in progress..."
@@ -51,15 +59,18 @@ cleanup(){
     fi
     exit 1
 }
+
 trap cleanup SIGINT
+
 echo "Simulating project setup... Press Ctrl+C to trigger cleanup."
 sleep 15
 
 echo "Project setup completed successfully."
 
 project_dir="attendance_tracker_$input"
+
 echo "Running health check"
-if python3 --version >/dev/null 2?&1; then
+if python3 --version >/dev/null 2>&1; then
 echo "Python3 is installed."
 else
 echo "Warning: Python3 is not installed."
